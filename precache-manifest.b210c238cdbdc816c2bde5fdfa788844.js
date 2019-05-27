65c88976e6aabca1765b8fe1911d7033e350ffbf
juliennesantos <juliennejsantos@gmail.com>
2019-05-27 12:09:34 +0800

    Updates

---
+++ precache-manifest.b210c238cdbdc816c2bde5fdfa788844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d5ea1e8a6059b8ed65f742dab9f1d9d",
+    "url": "/juliennesantos/portfolio/index.html"
+  },
+  {
+    "revision": "5f0c5d23672184267c3e",
+    "url": "/juliennesantos/portfolio/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "5081fda37ca784a96222",
+    "url": "/juliennesantos/portfolio/static/css/main.70167d20.chunk.css"
+  },
+  {
+    "revision": "5f0c5d23672184267c3e",
+    "url": "/juliennesantos/portfolio/static/js/2.0a9b9f73.chunk.js"
+  },
+  {
+    "revision": "5081fda37ca784a96222",
+    "url": "/juliennesantos/portfolio/static/js/main.4cf403d1.chunk.js"
+  },
+  {
+    "revision": "d3e41c3fde0c064cdb6e",
+    "url": "/juliennesantos/portfolio/static/js/runtime~main.3e7418e4.js"
+  }
+]);
