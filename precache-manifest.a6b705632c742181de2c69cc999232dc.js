ab6b1b789ca43d7b8fb6f131cf73fef7739fb075
juliennesantos <juliennejsantos@gmail.com>
2019-05-27 13:58:30 +0800

    Updates

--- precache-manifest.a6b705632c742181de2c69cc999232dc.js
+++ precache-manifest.a6b705632c742181de2c69cc999232dc.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "19094e9da7498ed7b939977ddc0a3916",
+    "revision": "21e6f61a21e64453de7fc1c87ab3ea7a",
     "url": "/juliennesantos/portfolio/index.html"
   },
   {
-    "revision": "5f0c5d23672184267c3e",
+    "revision": "47bcebf31b00a54572cf",
     "url": "/juliennesantos/portfolio/static/css/2.60a6b8e0.chunk.css"
   },
   {
-    "revision": "221dd2cea15547d130d4",
+    "revision": "bade440525394fdc3d76",
     "url": "/juliennesantos/portfolio/static/css/main.e59bf118.chunk.css"
   },
   {
-    "revision": "5f0c5d23672184267c3e",
-    "url": "/juliennesantos/portfolio/static/js/2.0a9b9f73.chunk.js"
+    "revision": "47bcebf31b00a54572cf",
+    "url": "/juliennesantos/portfolio/static/js/2.e9dde96a.chunk.js"
   },
   {
-    "revision": "221dd2cea15547d130d4",
-    "url": "/juliennesantos/portfolio/static/js/main.4cf403d1.chunk.js"
+    "revision": "bade440525394fdc3d76",
+    "url": "/juliennesantos/portfolio/static/js/main.2d8b441e.chunk.js"
   },
   {
     "revision": "d3e41c3fde0c064cdb6e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/juliennesantos/portfolio/precache-manifest.954390ed27afba1e90728bb83dec14b7.js"
+  "/juliennesantos/portfolio/precache-manifest.a6b705632c742181de2c69cc999232dc.js"
 );
 
 self.addEventListener('message', (event) => {
